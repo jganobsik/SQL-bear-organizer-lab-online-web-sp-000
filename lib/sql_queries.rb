5fa19d026dbb84c82a97f6755b7e6fd3b50a4354
Joe Ganobsik <jganobsik@gmail.com>
2019-01-03 20:49:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
- "SELECT color, COUNT(DISTINCT color COLLATE )
+ "SELECT color, COUNT(DISTINCT color COLLATE utf8mb4_unicode_ci)
     FROM bears"
 end
 
