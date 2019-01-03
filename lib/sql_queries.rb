87660c136b72610986f405fe873af08d04045605
Joe Ganobsik <jganobsik@gmail.com>
2019-01-03 20:40:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,9 +21,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
- "SELECT count(*) 
+ "SELECT count(*),  
     FROM bears
-    WHERE temperament = 'goofy'"
+    WHERE color = 'goofy'"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
